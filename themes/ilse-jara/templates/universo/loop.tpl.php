c776066d0ffd535f7d03981f664da1c48145c562
Wisquimas <wisquimas@gmail.com>
2017-05-18 17:47:01 -0500

    Limpieza

--- themes/ilse-jara/templates/universo/loop.tpl.php
+++ themes/ilse-jara/templates/universo/loop.tpl.php
@@ -9,23 +9,3 @@
    style="background-image: url(<?= $universo->imagenes_normal ?>)">
     <span class="Universo--loop--hover" style="background-image: url(<?= $universo->imagenes_hover ?>)"></span>
 </a>
-<a class="Universo--loop" href="<?= $universo->informacion_link ?>" target="_blank"
-   style="background-image: url(<?= $universo->imagenes_normal ?>)">
-    <span class="Universo--loop--hover" style="background-image: url(<?= $universo->imagenes_hover ?>)"></span>
-</a>
-<a class="Universo--loop" href="<?= $universo->informacion_link ?>" target="_blank"
-   style="background-image: url(<?= $universo->imagenes_normal ?>)">
-    <span class="Universo--loop--hover" style="background-image: url(<?= $universo->imagenes_hover ?>)"></span>
-</a>
-<a class="Universo--loop" href="<?= $universo->informacion_link ?>" target="_blank"
-   style="background-image: url(<?= $universo->imagenes_normal ?>)">
-    <span class="Universo--loop--hover" style="background-image: url(<?= $universo->imagenes_hover ?>)"></span>
-</a>
-<a class="Universo--loop" href="<?= $universo->informacion_link ?>" target="_blank"
-   style="background-image: url(<?= $universo->imagenes_normal ?>)">
-    <span class="Universo--loop--hover" style="background-image: url(<?= $universo->imagenes_hover ?>)"></span>
-</a>
-<a class="Universo--loop" href="<?= $universo->informacion_link ?>" target="_blank"
-   style="background-image: url(<?= $universo->imagenes_normal ?>)">
-    <span class="Universo--loop--hover" style="background-image: url(<?= $universo->imagenes_hover ?>)"></span>
-</a>
