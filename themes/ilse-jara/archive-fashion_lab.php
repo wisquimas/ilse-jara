90c1593b91ff46dc3fdec8858a2ca5e9cf02ccb6
Wisquimas <wisquimas@gmail.com>
2017-05-29 11:17:47 -0500

    FashionLab

---
+++ themes/ilse-jara/archive-fashion_lab.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Vista de fashionLab
+ * @return string
+ */
+$htmlFashionLab = function () {
+    $html = '';
+    $fashionLabs = \IlseJara\FashionLab::Get(array(
+        'posts_per_page' => -1,
+    ), true);
+
+    if (count($fashionLabs)) {
+        foreach ($fashionLabs as $lab) {
+            $html .= \Gafa\GafaTemplate::Imprimir('fashion-lab/loop', array(
+                'fashion' => $lab,
+            ));
+        }
+    }
+
+    return $html;
+};
+
+/*****************************************************
+ * Impresion
+ ****************************************************/
+get_header();
+
+echo \Gafa\GafaTemplate::Imprimir('fashion-lab/body', array(
+    'htmlFashionLab'  => $htmlFashionLab(),
+));
+
+echo \IlseJara\Footer::Imprimir();
+
+get_footer();
