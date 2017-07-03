0f410443c51be254eb528cc8edcbcf68ae7cd08a
vagrant <vagrant@servidorLocal>
2017-07-03 01:29:26 +0000

    Mejoras

--- wp/wp-content/themes/ilse-jara/templates/contacto/form.tpl.php
+++ wp/wp-content/themes/ilse-jara/templates/contacto/form.tpl.php
@@ -1,9 +1,8 @@
 <div class="Contacto">
     <div class="Contacto--selector">
         <div class="Contacto--selector--select">PRESUPUESTO</div>
-        <div class="Contacto--selector--select">CONSULTAS</div>
-        <div class="Contacto--selector--select">RELACIONES INTERNACIONALES VENTAS</div>
-        <div class="Contacto--selector--select">MARKETING</div>
+        <div class="Contacto--selector--select">Información</div>
+        <div class="Contacto--selector--select">RELACIONES INTERNACIONALES y VENTAS</div>
         <div class="Contacto--selector--select">TRABAJE CON NOSOTROS</div>
     </div>
     <div class="Contacto--ventana--body">
@@ -13,19 +12,37 @@
             <form action="" class="Contacto--form">
                 <input type="hidden" name="tipo" id="Contacto--form--tipo"/>
                 <div class="Contacto--form--label Contacto--form--label--email">
+                    <label for="contacto-nombre">Nombre</label>
+                    <div class="Contacto--form--input--cont">
+                        <input type="text" class="Contacto--form--input" name="Nombre" id="contacto-nombre"/>
+                    </div>
+                </div>
+                <div class="Contacto--form--label Contacto--form--label--email">
+                    <label for="contacto-apellido">Apellido</label>
+                    <div class="Contacto--form--input--cont">
+                        <input type="text" class="Contacto--form--input" name="Apellido" id="contacto-apellido"/>
+                    </div>
+                </div>
+                <div class="Contacto--form--label Contacto--form--label--email">
+                    <label for="contacto-pais">País</label>
+                    <div class="Contacto--form--input--cont">
+                        <input type="text" class="Contacto--form--input" name="País" id="contacto-pais"/>
+                    </div>
+                </div>
+                <div class="Contacto--form--label Contacto--form--label--email">
                     <label for="contacto-email">Mail</label>
                     <div class="Contacto--form--input--cont">
                         <input type="email" class="Contacto--form--input" name="Email" id="contacto-email"/>
                     </div>
                 </div>
-                <div class="Contacto--form--label Contacto--form--label--telefono">
+                <div class="Contacto--form--label Contacto--form--label--email">
                     <label for="contacto-telefono">Teléfono</label>
                     <div class="Contacto--form--input--cont">
                         <input type="tel" class="Contacto--form--input" name="Telefono" id="contacto-telefono"/>
                     </div>
                 </div>
                 <div class="clear"></div>
-                <textarea name="texto" id="" cols="30" rows="10" class="Contacto--form--textarea"></textarea>
+                <textarea name="texto" id="" cols="30" rows="10" class="Contacto--form--textarea" placeholder=""></textarea>
                 <button class="Contacto--form--enviar boton" type="button">Enviar</button>
             </form>
         </div>
